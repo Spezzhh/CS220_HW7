8ed643a89b1a38fed4715b81961d004d2f429eb2
Sami Ahmed <sami872344@gmail.com>
2023-11-14 01:25:59 -0500

    fetchCurrentTemperature v1

--- src/fetchCurrentTemperature.ts
+++ src/fetchCurrentTemperature.ts
@@ -8,5 +8,14 @@ interface TemperatureReading {
 
 export function fetchCurrentTemperature(coords: GeoCoord): Promise<TemperatureReading> {
   // TODO
-  return new Promise(res => res({ time: [], temperature_2m: [] }));
+  const url = `https://220.maxkuechen.com/currentTemperature/forecast?latitude=${coords.lat}&longitude=${coords.lon}&hourly=temperature_2m&temperature_unit=fahrenheit`;
+
+  return fetch(url)
+    .then((response: any) => response.json())
+    .then((data: TemperatureReading) => {
+      const time: string[] = data.time;
+      const temperature_2m: number[] = data.temperature_2m;
+
+      return { time, temperature_2m };
+    });
 }
