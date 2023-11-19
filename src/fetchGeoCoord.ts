ae5a22879e64a3e06d8d56a39b3b6f481a03f2a2
Sami Ahmed <sami872344@gmail.com>
2023-11-19 10:40:28 -0500

    attempt at university weather

--- src/fetchGeoCoord.ts
+++ src/fetchGeoCoord.ts
@@ -1,11 +1,40 @@
 import fetch from "../include/fetch.js";
+import { URL } from "url";
 
 export interface GeoCoord {
-  lat: number;
-  lon: number;
+    lat: number;
+    lon: number;
+}
+// Helper that builds a URL for the geoCoord API
+function makeGeoURL(query: string): string {
+    const searchURL = new URL("https://220.maxkuechen.com/geoCoord/search"); // base url for our API usage
+    searchURL.searchParams.append("q", (query));
+    return searchURL.toString();
 }
 
 export function fetchGeoCoord(query: string): Promise<GeoCoord> {
-  // TODO
-  return new Promise(res => res({ lat: NaN, lon: NaN }));
+    // AJ - Base implementation 11/16 & 11/17
+    const geoURL = makeGeoURL(query); // turn query str into url
+    console.log("URL being fetched:", geoURL); // url debugging print statement
+    return fetch(geoURL)
+        .then(apiResponse => {
+            if (!apiResponse.ok) {
+                throw new Error(apiResponse.statusText); // displaying error message
+            }
+            return apiResponse.json(); // grab response as json
+        })
+        .then(data => {
+            if (!Array.isArray(data) || data.length === 0) { // if data is not an array/is empty throw error
+                throw new Error("No results");
+            }
+
+            const first = data[0]; // first element in array, check if it has lon and lat values
+            if (typeof first !== 'object' || !('lon' in first) || !('lat' in first)) {
+                throw new Error('Invalid response format'); // if not throw error
+            }
+            return { // returning appropriate values (lon and lat)
+                lon: Number.parseFloat(first.lon),
+                lat: Number.parseFloat(first.lat)
+            };
+        });
 }
--- src/universityWeather.ts
+++ src/universityWeather.ts
@@ -1,3 +1,7 @@
+import { fetchUniversities } from './fetchUniversities';
+import { fetchGeoCoord, GeoCoord} from './fetchGeoCoord';
+import { fetchCurrentTemperature } from './fetchCurrentTemperature';
+
 interface AverageTemperatureResults {
   totalAverage: number;
   [key: string]: number;
@@ -8,6 +12,14 @@ export function fetchUniversityWeather(
   transformName?: (s: string) => string
 ): Promise<AverageTemperatureResults> {
   // TODO
+  
+  // fetch all the unis then check if array is valid then transform each uni and fetch the coords of that individual uni
+   fetchUniversities(universityQuery).then(unis => Array.isArray(unis) && unis.length === 0 ? unis: Promise.reject(new Error("No results found")))
+   .then(uni => {
+    const transformed = transformName ? transformName(uni) : uni;
+    return [uni, fetchGeoCoord(transformed)];
+  })
+
   return new Promise(res => res({ totalAverage: NaN }));
 }
 
