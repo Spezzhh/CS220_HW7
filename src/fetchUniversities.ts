2ecb48e41a7bc28cf6fa7dc438ea40d5d95675a2
Andrew <64822608+Spezzhh@users.noreply.github.com>
2023-11-21 12:33:06 -0500

    Update fetchUniversities.ts

--- src/fetchUniversities.ts
+++ src/fetchUniversities.ts
@@ -1,44 +1,17 @@
 import fetch from "../include/fetch.js";
-
+import { URL_Builder } from "./utility.js";
 interface Name {
-  name: string
+  name: string;
 }
-
 export function fetchUniversities(query: string): Promise<string[]> {
-
-  const makeSearchURL = (query: string): string => {
-      const searchURL = new URL("http://220.maxkuechen.com/universities/search?");
-      searchURL.searchParams.append("name", query);
-      return searchURL.toString();
-    };
-
-  const url: string = makeSearchURL(query);
-
-  return fetch(url)
-      .then(result => result.ok ? result.json() : Promise.reject(new Error("No results found for query.")))
-      .then(json => Array.isArray(json) && json.length > 0 ? json : Promise.reject(new Error("No results found for query.")))
-      .then((data: Name[]) => data.map((x: Name) => x.name))
-      .catch(error => Promise.reject(error));
-}
-import fetch from "../include/fetch.js";
-
-interface Name {
-  name: string
-}
-
-export function fetchUniversities(query: string): Promise<string[]> {
-
-  const makeSearchURL = (query: string): string => {
-      const searchURL = new URL("http://220.maxkuechen.com/universities/search?");
-      searchURL.searchParams.append("name", query);
-      return searchURL.toString();
-    };
-
-  const url: string = makeSearchURL(query);
-
-  return fetch(url)
-      .then(result => result.ok ? result.json() : Promise.reject(new Error("No results found for query.")))
-      .then(json => Array.isArray(json) && json.length > 0 ? json : Promise.reject(new Error("No results found for query.")))
-      .then((data: Name[]) => data.map((x: Name) => x.name))
-      .catch(error => Promise.reject(error));
+  // AJ (11/20) - Updated function to remove local URL builder function
+  //             and in its place, use the URL_Builder function from utility.ts
+  const searchURL: string = URL_Builder("http://220.maxkuechen.com/universities/search?", "name", query);
+  return fetch(searchURL)
+    .then(result => (result.ok ? result.json() : Promise.reject(new Error("No results found for query."))))
+    .then(json =>
+      Array.isArray(json) && json.length > 0 ? json : Promise.reject(new Error("No results found for query."))
+    )
+    .then((data: Name[]) => data.map((x: Name) => x.name))
+    .catch(error => Promise.reject(error));
 }
