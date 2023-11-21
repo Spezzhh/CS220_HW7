9db8854d260cf9dcc7970853e12ffd7dc6f11f69
Andrew <64822608+Spezzhh@users.noreply.github.com>
2023-11-21 12:32:07 -0500

    Update fetchCurrentTemperature.ts

--- src/fetchCurrentTemperature.ts
+++ src/fetchCurrentTemperature.ts
@@ -1,6 +1,6 @@
 import { GeoCoord } from "./fetchGeoCoord.js";
 import fetch from "../include/fetch.js";
-import { URL } from "url";
+import { URL_Builder } from "./utility.js";
 
 interface TemperatureReading {
   time: string[];
@@ -8,55 +8,21 @@ interface TemperatureReading {
 }
 
 export function fetchCurrentTemperature(coords: GeoCoord): Promise<TemperatureReading> {
-  // TODO
-
-  function makeSearchURL(latQuery: string, longQuery: string): string {
-    // Construct a new URL object using the resource URL
-    const searchURL = new URL(" https://220.maxkuechen.com/currentTemperature/forecast?");
-    searchURL.searchParams.append("latitude", latQuery);
-    searchURL.searchParams.append("longitude", longQuery);
-    searchURL.searchParams.append("hourly", "temperature_2m");
-    searchURL.searchParams.append("temperature_unit", "fahrenheit");
-
-    return searchURL.toString();
-  }
-
-  return fetch(makeSearchURL(String(coords.lat), String(coords.lon)))
-    .then((response: Response) => (response.ok ? response.json() : Promise.reject(new Error(response.statusText))))
-    .then((data: { hourly: TemperatureReading }) => {
-      const time: string[] = data.hourly.time;
-      const temperature_2m: number[] = data.hourly.temperature_2m;
-      return { time, temperature_2m };
-    });
-}
-import { GeoCoord } from "./fetchGeoCoord.js";
-import fetch from "../include/fetch.js";
-import { URL } from "url";
-
-interface TemperatureReading {
-  time: string[];
-  temperature_2m: number[];
-}
-
-export function fetchCurrentTemperature(coords: GeoCoord): Promise<TemperatureReading> {
-  // TODO
-
-  function makeSearchURL(latQuery: string, longQuery: string): string {
-    // Construct a new URL object using the resource URL
-    const searchURL = new URL(" https://220.maxkuechen.com/currentTemperature/forecast?");
-    searchURL.searchParams.append("latitude", latQuery);
-    searchURL.searchParams.append("longitude", longQuery);
-    searchURL.searchParams.append("hourly", "temperature_2m");
-    searchURL.searchParams.append("temperature_unit", "fahrenheit");
-
-    return searchURL.toString();
-  }
-
-  return fetch(makeSearchURL(String(coords.lat), String(coords.lon)))
-    .then((response: Response) => (response.ok ? response.json() : Promise.reject(new Error(response.statusText))))
-    .then((data: { hourly: TemperatureReading }) => {
-      const time: string[] = data.hourly.time;
-      const temperature_2m: number[] = data.hourly.temperature_2m;
-      return { time, temperature_2m };
-    });
-}
+    // (AJ 11/21) - Updated function to incorporate URL_Builder function from utility.ts
+    let temperatureURL: string = URL_Builder("https://220.maxkuechen.com/currentTemperature/forecast?", "latitude", String(coords.lat));
+    temperatureURL = URL_Builder(temperatureURL, "longitude", String(coords.lon));
+    temperatureURL = URL_Builder(temperatureURL, "hourly", "temperature_2m");
+    temperatureURL = URL_Builder(temperatureURL, "temperature_unit", "fahrenheit");
+    return fetch(temperatureURL)
+        .then((response: Response) => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then((data: { hourly: TemperatureReading }) => {
+            const time: string[] = data.hourly.time;
+            const temperature_2m: number[] = data.hourly.temperature_2m;
+            return { time, temperature_2m };
+        });
+    }
