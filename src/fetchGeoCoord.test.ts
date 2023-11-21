7f6f15ed3afcbd02857dc12d4c3328462eca92c0
Andrew <64822608+Spezzhh@users.noreply.github.com>
2023-11-21 12:32:27 -0500

    Update fetchGeoCoord.test.ts

--- src/fetchGeoCoord.test.ts
+++ src/fetchGeoCoord.test.ts
@@ -12,22 +12,13 @@ describe("fetchGeoCoord", () => {
       assert(Object.keys(result).length === 2); // Assert there are only two keys in the object
     });
   });
-});
-import assert from "assert";
-import { fetchGeoCoord } from "./fetchGeoCoord.js";
-
-jest.setTimeout(5000); // 5 second timeout
-
-
-describe("fetchGeoCoord", () => {
-  it("follows type specification", () => {
-    const promise = fetchGeoCoord("University of Massachusetts Amherst");
-
-    return promise.then(result => {
-      assert(typeof result === "object"); //  Assert the result is an object
-      assert(typeof result.lon === "number"); // Assert that the lon value is a number
-      assert(typeof result.lat === "number"); // Assert that the lat value is a number
-      assert(Object.keys(result).length === 2); // Assert there are only two keys in the object
-    });
+  // (AJ 11/21) - updated with a new test to reach coverage requirement
+  it("handles invalid coordinates", () => {
+    const invalid = "Invalid"; // blantantly invalid str value
+    return fetchGeoCoord(invalid).then( () => {
+      throw new Error("Test failed: Invalid coordinates provided to test");
+    })
+        .catch(error => {assert(error instanceof Error); // throw error for invalid coords as intended
+    })
   });
 });
